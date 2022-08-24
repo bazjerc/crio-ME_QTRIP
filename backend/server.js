f538155ce4bae4fd790db52bdc45c2d2dd9f2255
CrioUser <crio-user@criodo.com>
2022-08-24 12:36:56 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Kolkata");
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8082;
 
--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://43.205.18.163:8082" };
+const config = { backendEndpoint: "http://43.204.185.135:8082" };
 
 export default config;
